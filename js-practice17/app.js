6b7e5648ced9a9b9dec6bd9dcd27a91006ad9ab6
“Shehzad” <h.shehzad.ali@gmail.com>
2022-09-13 10:14:01 +0500

    form update

--- js-practice17/app.js
+++ js-practice17/app.js
@@ -15,31 +15,107 @@
 
 // var abc = { name: "Ghous", fName: "Ahmed" };
 
-function saveData() {
-  var data = document.querySelectorAll(".input");
-  console.log(data);
-  return data;
-  //   console.log(event.target.parentNode);
-}
-a = saveData();
+// const abcfunc = (a) => console.log(`hellow ${a}`);
+// const de = 5;
+// abcfunc(de);
 
-const abcfunc = (a) => console.log(`hellow ${a}`);
-const de = 5;
-abcfunc(de)
+// abcfunc(ab);
 
-abcfunc(ab);
-
-var ab = 4;
-
-
+// var ab = 4;
 
 // data get from api
 // fetch from jsonplaceholder
 
-fetch("https://jsonplaceholder.typicode.com/posts")
-  .then((result) => {
-    console.log("result is ", result);
-  })
-  .catch((err) => {
-    console.log("Error ===>", err);
-  });
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then((result) => {
+//     console.log("result is ", result);
+//   })
+//   .catch((err) => {
+//     console.log("Error ===>", err);
+//   });
+
+// function abc() {
+//   let a = "hello";
+//   if (true) {
+//     console.log(a);
+//   }
+// }
+// abc();
+
+// function ab(a) {
+//   return (b) => {
+//     return (c) => {
+//       return a + b + c;
+//     };
+//   };
+// }
+
+// function Abc(fName, dob, email) {
+//   this.myName = fName;
+//   this.myDOB = dob;
+//   this.myEmail = email;
+// }
+// function Abc(fName, dob, email) {
+//   this.myName = fName;
+//   this.myDOB = dob;
+//   this.myEmail = email;
+// }
+
+// var objd = new Abc("shehzad", "1234", "h@gmail.com");
+// console.log(objd);
+
+// ab(1)(2)(3);
+// console.log(ab(1)(2)(3));
+
+let name1 = document.getElementById("names"),
+  fName = document.getElementById("fatherName"),
+  email = document.getElementById("email"),
+  mobile = document.getElementById("mobile"),
+  cnic = document.getElementById("cnic"),
+  course = document.getElementById("course"),
+  gender = document.getElementById("gender"),
+  file = document.getElementById("file"),
+  city = document.getElementById("city"),
+  counter = 0;
+
+console.log(name1);
+
+class InputData {
+  constructor(names, fName, email, mobile, cnic, course, city, img, gender) {
+    this.nameInp = names;
+    this.fatherName = fName;
+    this.email = email;
+    this.phone = mobile;
+    this.cnic = cnic;
+    this.course = course;
+    this.city = city;
+    this.image = img;
+    this.gender = gender;
+  }
+}
+
+function checkField() {
+  console.log(event.target.value);
+}
+
+function counterData() {
+  let checkStorage = localStorage.getItem("counter");
+  if (!checkStorage) {
+    counter = checkStorage;
+  }
+}
+
+function saveData() {
+  var data = document.getElementsByClassName(".input");
+  console.log(data);
+  for (let index = 0; index < data.length; index++) {
+    const element = data[index];
+
+    console.log(element);
+  }
+  counter++;
+  localStorage.counter = counter;
+  //   console.log(event.target.parentNode);
+}
+
+// a = saveData();
